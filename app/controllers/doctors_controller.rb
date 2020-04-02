2df71d03d0faa0e73c46b16a1e18c405140cc749
rfgr87 <royguzrod@gmail.com>
2020-04-01 21:10:31 -0400

    modified doctor_patient_communication_app

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -68,7 +68,7 @@ class DoctorsController < ApplicationController
     @doctor.update(params["doctor"])
     redirect "doctors/#{@doctor.id}"
   end
-
+end
 #Idea for login based on the sessios lab
 
 
@@ -77,82 +77,75 @@ class DoctorsController < ApplicationController
 
 
 #Code from User Lab to do with sessions
-post '/login' do
-  @user = User.find_by(username: params[:username])
-  if @user
-    session[:user_id] = @user.id 
-    redirect to '/account'
-  else
-    erb :error
-  end
-end
+# post '/login' do
+#   @user = User.find_by(username: params[:username])
+#   if @user
+#     session[:user_id] = @user.id 
+#     redirect to '/account'
+#   else
+#     erb :error
+#   end
+# end
 
-get '/account' do
-  erb :account
-end
+# get '/account' do
+#   erb :account
+# end
 
 
+# #Other lab code
 
-end
+#   get "/signup" do
+#     erb :signup
+#   end
 
-#Other lab code
-
-  get "/signup" do
-    erb :signup
-  end
-
-  post "/signup" do
-    if params[:password] == "" || params[:username] == ""
-      redirect "/failure"
-    else
-      User.create(username: params[:username], password: params[:password])
-      redirect "/login"
-    end
-  end
+#   post "/signup" do
+#     if params[:password] == "" || params[:username] == ""
+#       redirect "/failure"
+#     else
+#       User.create(username: params[:username], password: params[:password])
+#       redirect "/login"
+#     end
+#   end
   
 
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
+#   get '/account' do
+#     @user = User.find(session[:user_id])
+#     erb :account
+#   end
 
 
-  get "/login" do
-    erb :login
-  end
+#   get "/login" do
+#     erb :login
+#   end
 
-  post "/login" do
-    user = User.find_by(username: params[:username])
+#   post "/login" do
+#     user = User.find_by(username: params[:username])
  
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/account"
-    else
-      redirect "/failure"
-    end
-  end
+#     if user && user.authenticate(params[:password])
+#       session[:user_id] = user.id
+#       redirect "/account"
+#     else
+#       redirect "/failure"
+#     end
+#   end
 
-  get "/failure" do
-    erb :failure
-  end
+#   get "/failure" do
+#     erb :failure
+#   end
 
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
+#   get "/logout" do
+#     session.clear
+#     redirect "/"
+#   end
 
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
+#   helpers do
+#     def logged_in?
+#       !!session[:user_id]
+#     end
 
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
+#     def current_user
+#       User.find(session[:user_id])
+#     end
+#   end
 
-end
-
-
-
-end
+# end
